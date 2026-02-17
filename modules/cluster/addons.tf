c9f4946ce68f6ea55fc7492811e70df5b7066971
Fabrício Nascimento <fabriciocostadn@gmail.com>
2026-02-17 20:18:43 -0300

    feat: vpc_cni prefix delegation

---
+++ modules/cluster/addons.tf
@@ -0,0 +1,15 @@
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name   = "vpc-cni"
+
+  addon_version = "v1.19.0-eksbuild.1"
+
+  configuration_values = jsonencode({
+    env = {
+      ENABLE_PREFIX_DELEGATION = "true"
+      WARM_PREFIX_TARGET       = "1"
+    }
+  })
+
+  depends_on = [aws_eks_cluster.eks-cluster]
+}
