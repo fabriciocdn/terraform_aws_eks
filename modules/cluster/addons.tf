3450980389d0c1d7f4a5d922e4bd06488d150769
Fabrício Nascimento <fabriciocostadn@gmail.com>
2026-02-17 21:23:35 -0300

    feat: add addon aws-ebs-csi

--- modules/cluster/addons.tf
+++ modules/cluster/addons.tf
@@ -13,3 +13,12 @@ resource "aws_eks_addon" "vpc_cni" {
 
   depends_on = [aws_eks_cluster.eks-cluster]
 }
+
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name   = "aws-ebs-csi-driver"
+
+  addon_version = "v1.39.0-eksbuild.1"
+
+  depends_on = [aws_eks_cluster.eks-cluster]
+}
