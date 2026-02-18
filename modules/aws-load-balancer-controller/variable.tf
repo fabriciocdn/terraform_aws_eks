17089f7ef04f1422a1370fb7d4c4628e5811cf6f
Fabrício Nascimento <fabriciocostadn@gmail.com>
2026-02-17 22:46:55 -0300

    fix: vars erro

--- modules/aws-load-balancer-controller/variable.tf
+++ modules/aws-load-balancer-controller/variable.tf
@@ -14,3 +14,12 @@ variable "vpc_id" {
   type = string
 }
 
+variable "oidc_arn" {
+  type        = string
+  description = "ARN do provedor OIDC do cluster"
+}
+
+variable "oidc_url" {
+  type        = string
+  description = "URL do provedor OIDC (sem https://)"
+}
