44f6faa19e8697b6e9b6057ff8a7fa7d00f6696c
Fabrício Nascimento <fabriciocostadn@gmail.com>
2026-02-17 21:53:39 -0300

    feat: role to ebs csi

--- modules/cluster/addons.tf
+++ modules/cluster/addons.tf
@@ -20,7 +20,8 @@ resource "aws_eks_addon" "ebs_csi" {
 
   addon_version = "v1.39.0-eksbuild.1"
 
-  depends_on = [aws_eks_cluster.eks-cluster]
+  service_account_role_arn = aws_iam_role.ebs_csi_role.arn
+  depends_on               = [aws_eks_cluster.eks-cluster]
 }
 
 resource "kubernetes_storage_class_v1" "gp3" {
@@ -40,4 +41,32 @@ resource "kubernetes_storage_class_v1" "gp3" {
   }
 
   depends_on = [aws_eks_addon.ebs_csi]
+}
+
+
+resource "aws_iam_role" "ebs_csi_role" {
+  name = "ebs-csi-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.example.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.example.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy_att" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_role.name
 }
